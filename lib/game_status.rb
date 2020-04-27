4cc71d9ef39e8b12546f70d284a0273ecb75457a
immutable-integer-8885 <gidelbusto@gmail.com>
2020-04-27 01:25:06 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,8 +17,8 @@ WIN_COMBINATIONS = [
   ]
   
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
-    
+  WIN_COMBINATIONS.detect do |win_combination|
+#detect + find method
   win_index_1 = win_combination[0]
   win_index_2 = win_combination[1]
   win_index_3 = win_combination[2]
@@ -27,9 +27,8 @@ def won?(board)
   position_2 = board[win_index_2]
   position_3 = board[win_index_3]
   
-    if position_1 == position_2 && position_2 == position_3 && position_taken?(board,win_index_3)
+    position_1 == position_2 && position_2 == position_3 && position_taken?(board,win_index_3)
       return win_combination
-    end
   end
   return false
 end
